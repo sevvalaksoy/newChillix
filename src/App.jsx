3335412c997bc7adf8d95f82a06e5e7a51f924a6
sevvalaksoy <164859994+sevvalaksoy@users.noreply.github.com>
2024-06-11 19:43:26 +0300

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Link, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min'
 import './App.css'
+import Login from './pages/Login'
+import Welcome from './pages/Welcome'
+import Home from './pages/Home'
+import Header from './components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Switch>
+        <Route path="/" exact>
+          <h1>Welcome to Chillix</h1>
+        </Route>
+        <Route path="/login">
+          <Header/>
+          <Login/>
+        </Route>
+        <Route path="/welcome">
+          <Welcome/>
+        </Route>
+        <Route  path="/home">
+          <Header/>
+          <Home/>
+        </Route>
+      </Switch>
+      <div className='dev-navigation'>
+        <Link to="/">HomePage</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/welcome">Welcome</Link>
+        <Link to="/home">Home</Link>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
